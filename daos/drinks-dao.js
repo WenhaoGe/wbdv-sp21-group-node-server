2e1f2d884de04cb81ecaf4ed32b59bc9614742bd
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-04-15 00:49:41 -0400

    minor changes in naming

--- daos/drinks-dao.js
+++ daos/drinks-dao.js
@@ -1,9 +1,9 @@
 const drinksModel = require("../db/drinks/drinks-model")
 
 const findAllDrinks = () => drinksModel.find()
-const findDrinkByName = (drinkName) => drinksModel.find({"drinkName": drinkName})
+const findDrinkByName = (drinkName) => drinksModel.find({drinkName: drinkName})
 // const findDrinkById = (drinkID) => drinksModel.findById(drinkID)
-const findDrinkById = (drinkID) => drinksModel.find({"drinkID": drinkID})
+const findDrinkById = (drinkID) => drinksModel.find({drinkID: drinkID})
 const createADrink = (drink) => drinksModel.create(drink)
 
 module.exports = {
